0382c0b813f67981c8f3c64643bdc5fbabfdbbfe
BVASQUEZ\bvasq <bvasquezv>
2025-08-06 11:06:08 -0500

    Separar logica de guardado en base de datos con guardado fisico

--- src/main/java/com/ccadmin/app/schooldata/service/ImageStorageService.java
+++ src/main/java/com/ccadmin/app/schooldata/service/ImageStorageService.java
@@ -17,6 +17,7 @@ import java.util.UUID;
 public class ImageStorageService extends SessionService {
 
     private static final String IMAGE_FOLDER = "data/tesis";
+    private static final Boolean flgSavePhysicalPath = false;
 
     @Autowired
     private ExerciseImageRepository exerciseImageRepository;
@@ -29,11 +30,10 @@ public class ImageStorageService extends SessionService {
     public String saveImage(String base64DataUrl) throws IOException {
         // Separa el encabezado "data:image/xxx;base64,"
         String base64 = base64DataUrl.substring(base64DataUrl.indexOf(',') + 1);
-        byte[] bytes = Base64.getDecoder().decode(base64);
         String filename = UUID.randomUUID().toString() + ".png";
-        /*Path target = Paths.get(IMAGE_FOLDER, filename);
-        Files.createDirectories(target.getParent());
-        Files.write(target, bytes);*/
+        if(flgSavePhysicalPath){
+            this.savePhysicalPath(filename,base64);
+        }
         ExerciseImageEntity exerciseImage = new ExerciseImageEntity(filename,base64);
         exerciseImage.addSession(getUserCod());
         this.exerciseImageRepository.save(exerciseImage);
@@ -46,15 +46,31 @@ public class ImageStorageService extends SessionService {
      */
     public String loadImageAsBase64(String filename) throws IOException {
         String base64 = "";
-        /*Path file = Paths.get(IMAGE_FOLDER, filename);
-        byte[] bytes = Files.readAllBytes(file);
-        String base64 = Base64.getEncoder().encodeToString(bytes);*/
+
+        if(flgSavePhysicalPath){
+            base64 = this.findPhysicalPath(filename);
+            return "data:image/png;base64," + base64;
+        }
+
         try{
             base64 = this.exerciseImageRepository.findById(filename).get().ImageBase64;
         }catch (Exception ex){
             base64 = this.exerciseImageRepository.findById("b3d0b104-7492-42d1-87a5-16d625de5433.png").get().ImageBase64;
         }
+        return "data:image/png;base64," + base64;
+    }
 
+    private void savePhysicalPath(String filename,String base64) throws IOException {
+        byte[] bytes = Base64.getDecoder().decode(base64);
+        Path target = Paths.get(IMAGE_FOLDER, filename);
+        Files.createDirectories(target.getParent());
+        Files.write(target, bytes);
+    }
+
+    private String findPhysicalPath(String filename) throws IOException {
+         Path file = Paths.get(IMAGE_FOLDER, filename);
+        byte[] bytes = Files.readAllBytes(file);
+        String base64 = Base64.getEncoder().encodeToString(bytes);
         return "data:image/png;base64," + base64;
     }
 }
